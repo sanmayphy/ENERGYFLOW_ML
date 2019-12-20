d13f4fe02bd489c9b82a2e34e36b3950f08000b0
Lorenzo Santi <lox@MacBook-Air-di-Lorenzo.local>
2019-12-20 16:11:11 +0100

    update geometry in cluster

--- DATA_PROCESSING/readTree_Topo.py
+++ DATA_PROCESSING/readTree_Topo.py
@@ -15,49 +15,93 @@ import os
 #    os.remove('Outfile_TopoClusterTotal.h5')
 #else:
 #    print("Can not delete the file as it doesn't exists")
-#    print("")
-#    print('')
-f = h5py.File('../Outfile_CellInformation_HomDet_2to5GeV.h5','r')
+
+f = h5py.File('../Outfile_CellInformation_HomDet_15to20GeV_FineGran.h5','r')
 
 #print( f.keys()  )
 
 
 print ("Init")
 
-#neutral Dict
-Layer={}
-for LAY in range(1,7):
-    Layer['n'+str(LAY)]=(f['RealRes_ChargedEnergy_Layer'+str(LAY)][:])
+#choose kind of topoclusters ("Charged, Total, Neutral")
 
-#print(RealRes_NeutralEnergy_Layer1, 'layer1', 'AA')
-#print(RealRes_NeutralEnergy_Layer1,'ai')
+KIND="Total"
+
+#choose if add noise
+AddNoise=False
+
+noise=[13.24,  8.48, 16.95, 13.55,  8.12, 13.67]
+layers=[64,64,32,16,16,8]
+
+
+
+fnew_ch={}
+if AddNoise==False:
+    print("Creating calo for cluster on Total Energy + Noise")
+    for LAY in range(1,7):
+        Calo_T=np.zeros((len(f['Smeared_Track_Energy']), layers[LAY-1], layers[LAY-1]))
+        for ev in range(len(f['Smeared_Track_Energy'])):
+            Calo_T[ev]=(f['RealRes_'+str(KIND)+'Energy_Layer'+str(LAY)][ev][0])
+        fnew_ch['layer_'+str(LAY)]=Calo_T
+
 #out_image = [out_image1,out_image2,out_image3,out_image4,out_image5,out_image6]
 #out_image = np.stack(out_image,axis = 0)
+
+
+
+if (KIND=="Charged" or KIND =="Neutral") and AddNoise==True:
+    for lay in range(1,7):
+        Calo_Ch=np.zeros((len(f['Smeared_Track_Energy']), layers[lay-1], layers[lay-1]))
+        Calo_Ne=np.zeros((len(f['Smeared_Track_Energy']), layers[lay-1], layers[lay-1]))
+        
+        for ev in range(len(f['Smeared_Track_Energy'])):
+            Calo_Ch[ev] = f['RealRes_'+str(KIND)+'Energy_Layer'+str(lay)][ev][0] + np.random.normal(0, noise[lay-1], (layers[lay-1],layers[lay-1]))
+            
+        fnew_ch["layer_"+str(lay)]= Calo_Ch
+            
 out_image = []
 i = -1
 print ("Looping")
-for event in range(len(Layer['n5'])):
+for event in range(len(f['Smeared_Track_Energy'])):
 
     i = i+ 1
-    if i%10==0: print('in loop ev: ',i)
-    if i > 1000: break
-    #print(Layer['n5'][i])
-    Proto = tools_generic.Clustering(Layer['n1'][i],Layer['n2'][i],Layer['n3'][i],Layer['n4'][i],Layer['n5'][i],Layer['n6'][i])
-    #print('event'+str(event))
+    if i%50==0: print('in loop ev: ',i)
+    if i >10: break
+    #print(Layer['n1'][i])
+    Proto = tools_generic.Clustering( fnew_ch["layer_1"][i],
+                                      fnew_ch["layer_2"][i],
+                                      fnew_ch["layer_3"][i],
+                                      fnew_ch["layer_4"][i],
+                                      fnew_ch["layer_5"][i],
+                                      fnew_ch["layer_6"][i])
 
-    #print("AUA",Proto,len(Proto))
+
+    #print("Final proto",Proto,len(Proto))
     #print('')
     out_image.append(tools_generic.Assign_Topo(Proto))
 
+    
 
 
 
 out_image = np.array(out_image)
+
 print(out_image.shape)
+
 print('opening File')
-with h5py.File('Outfile_TopoClusterTotal.h5', 'w') as f1:
+with h5py.File('Outfile_TotalTopo_2N.h5', 'w') as f1:
      for layer_i in range(6):
         f1.create_dataset('TopoClusters' + str(layer_i+1), data=out_image[:, layer_i:layer_i+1,:, :])
+
+
+
+if (KIND=="Charged" or KIND =="Neutral") and AddNoise==True:
+    print('opening File 2')
+    my_h5=h5py.File('Outfile_CellInformation_Noisy'+str(KIND)+'.h5', 'w')
+
+
+    for lay in range(6):
+        my_h5.create_dataset(str(KIND)+'_'+str(lay+1) , data=fnew_ch["layer_"+str(lay+1)])
 
 print ("Exiting ... Bye!")
 #
--- DATA_PROCESSING/tools_generic.py
+++ DATA_PROCESSING/tools_generic.py
@@ -11,32 +11,36 @@ def Clustering(layer1,layer2,layer3,layer4,layer5,layer6):
  SeedP = Convert(layer1,layer2,layer3,layer4,layer5,layer6,0,2)
 
  #print("printing seedS",SeedS)  
- #print("printing seedN",SeedN)
- #print("printing seedP",SeedP)
+
  PC = Proto(SeedS,SeedN,SeedP)
  #print('PC before merge',len(PC), PC)
  if len(PC)==1: PCm = PC 
  else: PCm = MergeS(PC,SeedS)
 
  #print("PC after merge",len(PC), PCm)
-# print("PCC",PCm)
  return PCm
 
 
 
 def Convert(layer1,layer2,layer3,layer4,layer5,layer6,lower,upper):
  seed = []
-#noise=[0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
- noise = [20, 20, 30, 80, 80, 160]
 
+ #!!!!
+ #noise threshold for Noisless sample
+ #noise=[1, 1, 1, 1, 1, 1]
+
+ #noise threshold for Noisy samples
+ noise = [13.24,  8.48, 16.95, 13.55,  8.12, 13.67]
+
+ 
 # print(layer1[0])
  layer = []
- layer.append(layer1[0].tolist())
- layer.append(layer2[0].tolist())
- layer.append(layer3[0].tolist())
- layer.append(layer4[0].tolist())
- layer.append(layer5[0].tolist())
- layer.append(layer6[0].tolist())
+ layer.append(layer1.tolist())
+ layer.append(layer2.tolist())
+ layer.append(layer3.tolist())
+ layer.append(layer4.tolist())
+ layer.append(layer5.tolist())
+ layer.append(layer6.tolist())
 
  #print('layer 5',layer[4])
  #print("En. Check")
@@ -68,8 +72,10 @@ def Proto(sedS,sedN,sedP):
     proto_ensemble = []
 
     #check 2 neighbor if ATL==1 else check 1 neighbor
-    ATL = 1
-    
+
+    ATLS=True
+
+    ATLP=False
     for cell in sedS:
      proto = []
      cell_proto = []
@@ -80,57 +86,17 @@ def Proto(sedS,sedN,sedP):
      #define the one close to it
      #need to add new ones
 
-     
-     #+1 layer doubled
-     if lay == 0:
-      cell_proto.append([lay+1,2*cX+1,2*cY+1])
-      cell_proto.append([lay+1,2*cX+1,2*cY])
-      cell_proto.append([lay+1,2*cX,2*cY+1])
-      cell_proto.append([lay+1,2*cX,2*cY])
+     layers=[64,64,32,16,16,8]
 
-     if lay == 3:
-      cell_proto.append([lay+1,cX,cY])
+     #print("CELL", cell)
+     if lay!=5:
+      nextl(lay,proto,"next",cX,cY,ATLS)
+     if lay!=0:
+      nextl(lay,proto,"prev",cX,cY,ATLS)
 
-     if lay == 4:
-       cell_proto.append([lay-1,cX,cY])
-     #-1 layer doubled 
-     if lay==5 or lay ==2 or lay ==3:
-      cell_proto.append([lay-1,2*cX+1,2*cY+1])
-      cell_proto.append([lay-1,2*cX+1,2*cY])
-      cell_proto.append([lay-1,2*cX,2*cY+1])
-      cell_proto.append([lay-1,2*cX,2*cY])
-
-     #+1 layer half 
-     if lay==1 or lay ==2  or lay ==4:
-      cell_proto.append([lay+1,int(cX/2//1),int(cY/2//1)])
-
-     #-1 layer half
-     if lay == 1:
-      cell_proto.append([lay-1,int(cX/2//1),int(cY/2//1)])
-
-     cell_proto.append([lay,cX+1,cY])
-     cell_proto.append([lay,cX-1,cY])
-     cell_proto.append([lay,cX,cY+1])
-     cell_proto.append([lay,cX,cY-1])
-     cell_proto.append([lay,cX+1,cY+1])
-     cell_proto.append([lay,cX+1,cY-1])
-     cell_proto.append([lay,cX-1,cY+1])
-     cell_proto.append([lay,cX-1,cY-1])
-
-     if ATL == 1:
-      cell_proto.append([lay,cX+2,cY])
-      cell_proto.append([lay,cX-2,cY])
-      cell_proto.append([lay,cX,cY+2])
-      cell_proto.append([lay,cX,cY-2])
-      cell_proto.append([lay,cX+1,cY+2])
-      cell_proto.append([lay,cX+2,cY+1])
-      cell_proto.append([lay,cX+1,cY-2])
-      cell_proto.append([lay,cX+2,cY-1])
-      cell_proto.append([lay,cX-1,cY+2])
-      cell_proto.append([lay,cX-2,cY+1])
-      cell_proto.append([lay,cX-1,cY-2])
-      cell_proto.append([lay,cX-2,cY-1])
-
+     nextl(lay,proto,"same",cX,cY,ATLS)
+  
+     #print("NB",proto)
      
      #first cluster the low energy ones
      for cp in cell_proto:
@@ -147,66 +113,11 @@ def Proto(sedS,sedN,sedP):
        cXNP  = cpN[1]
        cYNP  = cpN[2]
        new_CellP = []
-       if layNP == 0:
-        new_CellP.append([layNP+1,2*cXNP+1,2*cYNP+1])
-        new_CellP.append([layNP+1,2*cXNP+1,2*cYNP])
-        new_CellP.append([layNP+1,2*cXNP,2*cYNP+1])
-        new_CellP.append([layNP+1,2*cXNP,2*cYNP])
 
-       if layNP==2:
-        new_CellP.append([layNP-1,2*cXNP+1,2*cYNP+1])
-	new_CellP.append([layNP-1,2*cXNP+1,2*cYNP])
-        new_CellP.append([layNP-1,2*cXNP,2*cYNP+1])
-        new_CellP.append([layNP-1,2*cXNP,2*cYNP])
-        
-        new_CellP.append([layNP+1,cXNP/2//1,cYNP/2//1])
-
-       if layNP==1:
-        new_CellP.append([layNP+1,int(cXNP/2//1),int(cYNP/2//1)])
-        new_CellP.append([layNP-1,int(cXNP/2//1),int(cYNP/2//1)])
-
-       if layNP ==3:
-        new_CellP.append([layNP-1,2*cXNP+1,2*cYNP+1])
-        new_CellP.append([layNP-1,2*cXNP+1,2*cYNP])
-        new_CellP.append([layNP-1,2*cXNP,2*cYNP+1])
-        new_CellP.append([layNP-1,2*cXNP,2*cYNP])
-
-        new_CellP.append([layNP+1,cXNP,cYNP]) 
-
-       if layNP ==4:
-        new_CellP.append([layNP-1,cXNP,cYNP])
-
-        new_CellP.append([layNP+1,int(cXNP/2//1),int(cYNP/2//1)])
-
-       if lay ==5:
-        new_CellP.append([layNP-1,2*cXNP+1,2*cYNP+1])
-	new_CellP.append([layNP-1,2*cXNP+1,2*cYNP])
-        new_CellP.append([layNP-1,2*cXNP,2*cYNP+1])
-        new_CellP.append([layNP-1,2*cXNP,2*cYNP])
-        
-
-       new_CellP.append([layNP,cXNP+1,cYNP])
-       new_CellP.append([layNP,cXNP-1,cYNP])
-       new_CellP.append([layNP,cXNP,cYNP+1])
-       new_CellP.append([layNP,cXNP,cYNP-1])
-       new_CellP.append([layNP,cXNP+1,cYNP+1])
-       new_CellP.append([layNP,cXNP+1,cYNP-1])
-       new_CellP.append([layNP,cXNP-1,cYNP+1])
-       new_CellP.append([layNP,cXNP-1,cYNP-1])
-
-       #if ATL==1:
-        #new_CellP.append([layNP,cXNP+2,cYNP])                                                                                                                                                             
-        #new_CellP.append([layNP,cXNP-2,cYNP])                                                                                                                                                             
-        #new_CellP.append([layNP,cXNP,cYNP+2])                                                                                                                                                              
-        #new_CellP.append([layNP,cXNP,cYNP-2]) 
-        #new_CellP.append([layNP,cXNP+1,cYNP+2])
-        #new_CellP.append([layNP,cXNP+2,cYNP+1])
-        #new_CellP.append([layNP,cXNP+1,cYNP-2])
-        #new_CellP.append([layNP,cXNP-2,cYNP+1])
-        #new_CellP.append([layNP,cXNP-1,cYNP+2])
-        #new_CellP.append([layNP,cXNP+2,cYNP-1])
-        #new_CellP.append([layNP,cXNP-1,cYNP-2])
-        #new_CellP.append([layNP,cXNP-2,cYNP-1])
+       if layNP!=5: nextl(layNP,new_CellP,"next",cXNP,cYNP,ATLP)
+       if layNP!=0: nextl(layNP,new_CellP,"prev",cXNP,cYNP,ATLP)
+       nextl(layNP,new_CellP,"same",cXNP,cYNP,ATLP)
+       
        
        for new_cell in new_CellP:
         if new_cell not in proto:
@@ -218,46 +129,12 @@ def Proto(sedS,sedN,sedP):
      #print('protoensamble',proto)
      proto_ensemble.append(proto)
     #need now to merge
-     
-
-    #print("PROTOENSEMBLE B",proto_ensemble)
-
-    #l1=len(proto_ensemble)
-    #l2=-1
+ 
     for i in range(50):
-     #l1=len(proto_ensemble)
      proto_ensemble=SMERGE(proto_ensemble)
      proto_ensemble=clean_duplicates(proto_ensemble)
-     #l2=len(proto_ensemble)
 
-    #print("Proto A",proto_ensemble, len(proto_ensemble))
-     
-    #merge_final = []
-    #merge_final_check = []
-   
-    #i = 0
-    #f = 0
-    #for pe in proto_ensemble:
-    # i = i +1
-    # merged = pe
-    # j = 0
-    # if pe not in merge_final_check:
-    #  for pe_1 in proto_ensemble:
-    #   j = j +1
-    #   if pe_1 != pe and pe_1 not in merge_final_check:
-    #    merged,m_app1,m_app2 = common_cluster(merged,pe_1)
-    #    if merged != m_app1:
-    #    merge_final_check.append(pe)
-    #     merge_final_check.append(pe_1)
-    #  if merged != []: f = f + 1
-    #  if merged != []: merge_final.append(merged)
-
-#        merged = merged + common_cluster(pe,pe_1)
-#      proto_ensembleMerged.append(merged)
-#
-    #merge_clean = clean_duplicates(proto_ensamble)
-    #print("CLEANN",merge_clean)
-
+  
 
     return proto_ensemble
 
@@ -266,88 +143,75 @@ def MergeS(proto,seed):
  lung1=len(proto)
  lung2=-1
  #print(len(proto))
- for i in range(1000):
-  lung1=len(proto)
-  Proto=Iter(proto,seed)
+ for i in range(50):
+  proto=Iter_2(proto,seed)
   proto=clean_duplicates(proto)
-  lung2=len(proto)
-  if lung1==lung2: break
 
  return proto
 
 
-
-
-
-
-# p_check = []
-# p_f = []
-# appo = []
-# for p in proto:
-#  pn = p
-#  p_check.append(p)
- # for p1 in proto:
- #  if p1 != p and p1 not in p_check:
- #   cell1= []
- #   for c in p1:
- #    if c[0] == 0:
- #     cell1.append([c[0]+1,2*c[1]+1,2*c[2]+1])
- #     cell1.append([c[0]+1,2*c[1]+1,2*c[2]])
- #     cell1.append([c[0]+1,2*c[1],2*c[2]+1])
- #     cell1.append([c[0]+1,2*c[1],2*c[2]])
-      
- #    if c[0]==5 or c[0] ==2 or c[0] ==3:
- #     cell1.append([c[0]-1,2*c[1]+1,2*c[2]+1])
- #     cell1.append([c[0]-1,2*c[1]+1,2*c[2]])
- #     cell1.append([c[0]-1,2*c[1],2*c[2]+1])
- #     cell1.append([c[0]-1,2*c[1],2*c[2]])
-      
-#     if c[0]==1 or c[0] ==2 or c[0] ==4:
-      #cell1.append([c[0]+1,c[1]/2//1,c[2]/2//1]) 
+def nextl(l,prot,which, X, Y, ATL):
+    
+    if which=="same":
+        prot.append([l,X+1 ,Y+1])
+        prot.append([l,X   ,Y+1])
+        prot.append([l,X-1 ,Y+1])
+        prot.append([l,X-1 ,Y  ])
+        prot.append([l,X-1 ,Y-1])
+        prot.append([l,X   ,Y-1])
+        prot.append([l,X+1 ,Y-1])
+        prot.append([l,X+1 ,Y  ])
         
-     #if c[0] == 1:
-      #cell1.append([c[0]-1,c[1]/2//1,c[2]/2//1])
-      
-  #   if c[0] == 3:
-  #    cell1.append([c[0]+1,c[1],c[2]])
-        
-  #   if c[0] == 4:
-  #    cell1.append([c[0]-1,c[1],c[2]])
-      
-   #  cell1.append([c[0],c[1]+1,c[2]])
-  #   cell1.append([c[0],c[1]-1,c[2]])
-  #   cell1.append([c[0],c[1],c[2]+1])
-  #   cell1.append([c[0],c[1],c[2]-1])
-
-#    print("BOUNDARY",cell1)
-     
-   # for neib in cell1:
-   #  if neib in p and neib in seed:
-     # pn=pn+p1
-     #else
-      #pf.append(p1)
-
-    #pf.append(pn)
-
-      
-
-      
-  #    p_f.append(p + p1)
-  #    p_check.append(p)
-  #    p_check.append(p1)
-  #    break
-# if p_f ==[]: p_f = proto
+        if ATL==True:
+            prot.append([l,X+2 ,Y  ])
+            prot.append([l,X+2 ,Y+1])
+            prot.append([l,X+1 ,Y+2])
+            prot.append([l,X   ,Y+2])
+            prot.append([l,X-1 ,Y+2])
+            prot.append([l,X-2 ,Y+1])
+            prot.append([l,X-2 ,Y  ])
+            prot.append([l,X-2 ,Y-1])
+            prot.append([l,X-1 ,Y-2])
+            prot.append([l,X   ,Y-2])
+            prot.append([l,X+1 ,Y-2])
+            prot.append([l,X+2 ,Y-1])
+        return
+       
+    layers=[64,64,32,16,16,8]
+    if which=="next": 
+     rat=layers[l+1]/float(layers[l])
+     l=l+1
+   
+    elif which=="prev":
  
-# return p_f
-     
+        rat=layers[l-1]/float(layers[l])
+        l=l-1
+
+    if rat==2:
+        prot.append([l,2*X+1 ,2*Y+1])
+        prot.append([l,2*X ,  2*Y+1])
+        prot.append([l,2*X+1 ,2*Y])
+        prot.append([l,2*X ,  2*Y])
+    if rat==1:
+
+        prot.append([l,X ,  Y])
+
+        
+    if rat==0.5:
+        prot.append([l,int(X/2//1),int(Y/2//1)])
+
+
+
+
+
+
 
 
 def common_cluster(proto1,proto2):
     protoO = proto1
     for cel in proto1:
      if cel in proto2:
-#     for cel2 in proto2:
-#      if cel == cel2:
+
        protoO = proto1+proto2
        break
     return protoO,proto1,proto2
@@ -494,59 +358,100 @@ def Iter(my_ens,sed):
 
 
 
+def Iter_2(my_ens,sed):
+ new_ens=[]
+ #check=[]
+ check=[0]*len(my_ens)
+ for i,p in enumerate(my_ens):
+  clus=p
+  for l,pp in enumerate(my_ens):
+   if l>i:
+
+    cell1=[]
+    for c in p:
+     #print([c[0]+1,2*c[1]+1,2*c[2]+1] not in p and [c[0]+1,2*c[1]+1,2*c[2]+1] not in cell1)                                                                                                               
+     if c[0] == 0:
+      if [c[0]+1,2*c[1]+1,2*c[2]+1] not in p and [c[0]+1,2*c[1]+1,2*c[2]+1] not in cell1: cell1.append([c[0]+1,2*c[1]+1,2*c[2]+1])
+      if [c[0]+1,2*c[1],2*c[2]+1]   not in p and [c[0]+1,2*c[1],2*c[2]+1]   not in cell1: cell1.append([c[0]+1,2*c[1],2*c[2]+1])
+      if [c[0]+1,2*c[1]+1,2*c[2]]   not in p and [c[0]+1,2*c[1]+1,2*c[2]]   not in cell1: cell1.append([c[0]+1,2*c[1]+1,2*c[2]])
+      if [c[0]+1,2*c[1],2*c[2]]     not in p and [c[0]+1,2*c[1],2*c[2]]     not in cell1: cell1.append([c[0]+1,2*c[1],2*c[2]] )
+     if c[0]==5 or c[0] ==2 or c[0] ==3:
+      if [c[0]-1,2*c[1]+1,2*c[2]+1] not in p and [c[0]-1,2*c[1]+1,2*c[2]+1] not in cell1: cell1.append([c[0]-1,2*c[1]+1,2*c[2]+1])
+      if [c[0]-1,2*c[1],2*c[2]+1]   not in p and [c[0]-1,2*c[1],2*c[2]+1]   not in cell1: cell1.append([c[0]-1,2*c[1],2*c[2]+1])
+      if [c[0]-1,2*c[1]+1,2*c[2]]   not in p and [c[0]-1,2*c[1]+1,2*c[2]]   not in cell1: cell1.append([c[0]-1,2*c[1]+1,2*c[2]])
+      if [c[0]-1,2*c[1],2*c[2]]     not in p and [c[0]-1,2*c[1],2*c[2]]     not in cell1: cell1.append([c[0]-1,2*c[1],2*c[2]])
+     if c[0]==1 or c[0] ==2 or c[0] ==4:
+      if [c[0]+1,int(c[1]/2//1),int(c[2]/2//1)] not in p and [c[0]+1,int(c[1]/2//1),int(c[2]/2//1)] not in cell1: cell1.append([c[0]+1,int(c[1]/2//1),int(c[2]/2//1)] )
+     if c[0] == 1:
+      if [c[0]-1,int(c[1]/2//1),int(c[2]/2//1)] not in p and [c[0]-1,int(c[1]/2//1),int(c[2]/2//1)] not in cell1: cell1.append([c[0]-1,int(c[1]/2//1),int(c[2]/2//1)] )
+     if c[0] == 3:
+      if [c[0]+1,c[1],c[2]] not in p and [c[0]+1,c[1],c[2]] not in cell1: cell1.append([c[0]+1,c[1],c[2]])
+     if c[0] == 4:
+      if [c[0]-1,c[1],c[2]] not in p and [c[0]-1,c[1],c[2]] not in cell1: cell1.append([c[0]-1,c[1],c[2]])
+      if [c[0],c[1]+1,c[2]] not in p and [c[0],c[1]+1,c[2]] not in cell1: cell1.append([c[0],c[1]+1,c[2]])
+      if [c[0],c[1]-1,c[2]] not in p and [c[0],c[1]-1,c[2]] not in cell1: cell1.append([c[0],c[1]-1,c[2]])
+      if [c[0],c[1],c[2]+1] not in p and [c[0],c[1],c[2]+1] not in cell1: cell1.append([c[0],c[1],c[2]+1])
+      if [c[0],c[1],c[2]-1] not in p and [c[0],c[1],c[2]-1] not in cell1: cell1.append([c[0],c[1],c[2]-1])
+    cell2=[]
+    for c in pp:
+     if c[0] == 0:
+      if [c[0]+1,2*c[1]+1,2*c[2]+1] not in pp and [c[0]+1,2*c[1]+1,2*c[2]+1] not in cell2: cell2.append([c[0]+1,2*c[1]+1,2*c[2]+1])
+      if [c[0]+1,2*c[1],2*c[2]+1]   not in pp and [c[0]+1,2*c[1],2*c[2]+1]   not in cell2: cell2.append( [c[0]+1,2*c[1],2*c[2]+1])
+      if [c[0]+1,2*c[1]+1,2*c[2]]   not in pp and [c[0]+1,2*c[1]+1,2*c[2]]   not in cell2: cell2.append( [c[0]+1,2*c[1]+1,2*c[2]])
+      if [c[0]+1,2*c[1],2*c[2]]     not in pp and [c[0]+1,2*c[1],2*c[2]]     not in cell2: cell2.append( [c[0]+1,2*c[1],2*c[2]]  )
+     if c[0]==5 or c[0] ==2 or c[0] ==3:
+      if [c[0]-1,2*c[1]+1,2*c[2]+1] not in pp and [c[0]-1,2*c[1]+1,2*c[2]+1] not in cell2: cell2.append( [c[0]-1,2*c[1]+1,2*c[2]+1])
+      if [c[0]-1,2*c[1],2*c[2]+1]   not in pp and [c[0]-1,2*c[1],2*c[2]+1]   not in cell2: cell2.append( [c[0]-1,2*c[1],2*c[2]+1])
+      if [c[0]-1,2*c[1]+1,2*c[2]]   not in pp and [c[0]-1,2*c[1]+1,2*c[2]]   not in cell2: cell2.append( [c[0]-1,2*c[1]+1,2*c[2]])
+      if [c[0]-1,2*c[1],2*c[2]]     not in pp and [c[0]-1,2*c[1],2*c[2]]     not in cell2: cell2.append( [c[0]-1,2*c[1],2*c[2]])
+     if c[0]==1 or c[0] ==2 or c[0] ==4:
+      if [c[0]+1,int(c[1]/2//1),int(c[2]/2//1)] not in pp and [c[0]+1,int(c[1]/2//1),int(c[2]/2//1)] not in cell2: cell2.append( [c[0]+1,int(c[1]/2//1),int(c[2]/2//1)] )
+     if c[0] == 1:
+      if [c[0]-1,int(c[1]/2//1),int(c[2]/2//1)] not in pp and [c[0]-1,int(c[1]/2//1),int(c[2]/2//1)] not in cell2: cell2.append([c[0]-1,int(c[1]/2//1),int(c[2]/2//1)] )
+     if c[0] == 3:
+      if [c[0]+1,c[1],c[2]] not in pp and [c[0]+1,c[1],c[2]] not in cell2: cell2.append( [c[0]+1,c[1],c[2]])
+     if c[0] == 4:
+      if [c[0]-1,c[1],c[2]] not in pp and [c[0]-1,c[1],c[2]] not in cell2: cell2.append( [c[0]-1,c[1],c[2]]   )
+      if [c[0],c[1]+1,c[2]] not in pp and [c[0],c[1]+1,c[2]] not in cell2: cell2.append([c[0],c[1]+1,c[2]])
+      if [c[0],c[1]-1,c[2]] not in pp and [c[0],c[1]-1,c[2]] not in cell2: cell2.append([c[0],c[1]-1,c[2]])
+      if [c[0],c[1],c[2]+1] not in pp and [c[0],c[1],c[2]+1] not in cell2: cell2.append([c[0],c[1],c[2]+1])
+      if [c[0],c[1],c[2]-1] not in pp and [c[0],c[1],c[2]-1] not in cell2: cell2.append([c[0],c[1],c[2]-1])
+
+    for neibp in cell1:
+     if (neibp in pp and neibp in sed):
+      clus+=pp
+      check[l]+=1
+      break
+    for neibpp in cell2:
+     if(neibpp in p and neibpp in sed):
+      clus+=pp
+      check[l]+=1
+      break
+   if my_ens[l]==my_ens[-1] and check[i]==0:
+    new_ens.append(clus)
+
+ return new_ens
 
 
-
-
-
-
-
-def StepMerge(my_ens):
-    new_ens=[]
-    check=[]
-    k=0
-    for i,p in enumerate(my_ens):
-        check.append(p)
-        j=0  
-        for l,pp in enumerate(my_ens):
-            if p!=pp and pp not in check:
-                for neibpp in pp:
-                    
-                    #print(neibpp, l, (neibpp in p))
-                    if(neibpp in p ):
-                        #print(neibpp, l)
-                        new_ens.append(p+pp)
-                        check.append(pp)
-                        k+=1
-                        j+=1
-                        break
-                if j==0: 
-                    new_ens.append(pp)
-                    check.append(pp)
-  
-        if k==0 and p not in new_ens:
-            new_ens.append(p)
-    
-    return new_ens
 
 
 
 def SMERGE(my_ens):
-    new_ens=[]
-    check=[0]*len(my_ens)
-    for i,p in enumerate(my_ens):
-        clus=p
-        for l,pp in enumerate(my_ens):
-            if l>i:
-                for neibpp in pp:
-                    if(neibpp in p) :
-                        clus+=pp
-                        check[l]+=1
-                        break
-            if my_ens[l]==my_ens[-1] and check[i]==0:
-                new_ens.append(clus)
+ new_ens=[]
+ check=[0]*len(my_ens)
+ for i,p in enumerate(my_ens):
+  clus=p
+  for l,pp in enumerate(my_ens):
+   if l>i:
+    for neibpp in pp:
+     if(neibpp in p) :
+      clus+=pp
+      check[l]+=1
+      break
+   if my_ens[l]==my_ens[-1] and check[i]==0:
+    new_ens.append(clus)
     
-    return new_ens
+ return new_ens
    
 
 def MergeStep(my_ens):
