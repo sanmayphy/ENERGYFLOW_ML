751a921e83167f366e3bdcb583d93d63462e03e0
Francesco Armando Di Bello <francesco.armando.di.bello@cern.ch>
2020-02-11 15:32:57 +0100

    new eval

--- DATA_PROCESSING/Evaluate.py
+++ DATA_PROCESSING/Evaluate.py
@@ -9,7 +9,6 @@ import os
 import math
 import Track
 
-
 Events = 5000
 
 f = h5py.File('/afs/cern.ch/work/s/sanmay/public/Outfile_CellInformation_HomDet_10to15GeV_FineGran128_V1.h5')
@@ -18,23 +17,22 @@ f = h5py.File('/afs/cern.ch/work/s/sanmay/public/Outfile_CellInformation_HomDet_
 gT     = h5py.File('Outfile_2to5GeV_TotalTopo.h5','r')
 gEpred = h5py.File('Outfile_2to5GeV_TotalEpred.h5','r')
 
-#h_Epred_prof_tot= ROOT.TProfile2D("h_Epred_prof_tot","h_Epred_prof_tot",5000000,1.5e3,5.5e3,6,0,6) 
-h_Epred_prof_tot= ROOT.TProfile2D("h_Epred_prof_tot","h_Epred_prof_tot",50,1.5e3,5.5e3,6,0,6) 
+#h_Epred_prof_tot= ROOT.TProfile2D("h_Epred_prof_tot","h_Epred_prof_tot",5000000,1.5e3,5.5e3,6,0,6)
+h_Epred_prof_tot= ROOT.TProfile2D("h_Epred_prof_tot","h_Epred_prof_tot",50,1.5e3,5.5e3,6,0,6)
 
 C1=f["Trk_Theta"]
 C2=f["Trk_Phi"]
 C3=f["Trk_X_pos"]
 C4=f["Trk_Y_pos"]
 
-
 h_Pflow =  ROOT.TH1D("h_Pflow","h_Pflow",50,-2,2)
 h_Pflow1 =  ROOT.TH1D("h_Pflownew","h_Pflownew",50,-2,2)
 h_Energy =  ROOT.TH1D("h_Energy","h_Energy",200,-500,10000)
 h_Eneutral =  ROOT.TH1D("h_Eneutral","h_Eneutral",200,-500,10000)
 
-f1 = ROOT.TFile.Open("Epred_outN.root")
+#f1 = ROOT.TFile.Open("Epred_outN.root")
 #f1 = ROOT.TFile.Open("fpred_new_final.root")
-hProf_t = f1.Get("h_Epred_prof_tot")
+#hProf_t = f1.Get("h_Epred_prof_tot")
 layers=[64,32,32,16,16,8]
 out_image = []
 for ev in range(Events):
@@ -53,9 +51,9 @@ for ev in range(Events):
      Etopo_t =gEpred["PflowTC"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
      for X in range(layers[lay]):
        for Y in range(layers[lay]):
-        if Etopo_t[X][Y] != 0: 
-         TC = Etopo_t[X][Y] 
-         break 
+        if Etopo_t[X][Y] != 0:
+         TC = Etopo_t[X][Y]
+         break
 
 # print "TC number found:",TC
 
@@ -69,7 +67,7 @@ for ev in range(Events):
       Epred_tot1 =gEpred["PflowPredTot"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
       Epred_LFI =gEpred["PflowLFI"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
       Noise_Ev=f["RealRes_Noise_Layer"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
-       
+
       for X in range(layers[lay]):
         for Y in range(layers[lay]):
          if   Topo_Ev[X][Y] == TC and TC != 0:
@@ -84,17 +82,17 @@ for ev in range(Events):
            Eneutral = Neu_Ev[X][Y] + Eneutral
            EPflow = Epred_Ev[X][Y] + EPflow
            Ech = Ch_Ev[X][Y] + Ech
- if Ech!=0: h_Epred_prof_tot.Fill(ptr,lf,Ech/ptr) 
- Epred_tot = ptr*hProf_t.GetBinContent(hProf_t.GetXaxis().FindBin(ptr),hProf_t.GetYaxis().FindBin(lf))
+ if Ech!=0: h_Epred_prof_tot.Fill(ptr,lf,Ech/ptr)
+# Epred_tot = ptr*hProf_t.GetBinContent(hProf_t.GetXaxis().FindBin(ptr),hProf_t.GetYaxis().FindBin(lf))
 # print Epred_tot, Ech, EPflow, Ech/ptr
  if  Ech!=0 and Epred_tot!= 0 and EPflow!= 0 and Etot != 0 and Eneutral != 0 and int(EPflow) != int(Etot):
  #if Eneutral != 0 and Ech!=0 and TC != 0:
-   h_Energy.Fill( Ech  ) 
-   h_Eneutral.Fill( Eneutral  ) 
-#   print  (Epred_tot-pred),TC 
-   h_Pflow.Fill(((Etot-pred) - Eneutral)/Eneutral)   
-   h_Pflow1.Fill(((Etot-EPflow) - Eneutral)/Eneutral)   
-f_o=ROOT.TFile("Epred_out.root","recreate") 
+   h_Energy.Fill( Ech  )
+   h_Eneutral.Fill( Eneutral  )
+#   print  (Epred_tot-pred),TC
+   h_Pflow.Fill(((Etot-pred) - Eneutral)/Eneutral)
+   h_Pflow1.Fill(((Etot-EPflow) - Eneutral)/Eneutral)
+f_o=ROOT.TFile("Epred_out.root","recreate")
 h_Pflow.Write()
 h_Pflow1.Write()
 h_Energy.Write()
