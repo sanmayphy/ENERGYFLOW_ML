4a2c6da4c3a548d324cd0bdf9f3afdd8d826b6f6
Francesco Armando Di Bello <francesco.armando.di.bello@cern.ch>
2020-08-06 09:24:25 +0200

    Added script and macro

--- DATA_PROCESSING/Evaluate_All.py
+++ DATA_PROCESSING/Evaluate_All.py
@@ -11,13 +11,18 @@ import Track
 
 
 
-algorithms = [["Pflow","PflowPred","/afs/cern.ch/work/f/fdibello/Outfile_2to5GeV_TotalEpred.h5"],
-              ["CNN"  ,"RealRes_NeutralEnergyPred_Layer","/afs/cern.ch/work/s/sanmay/public/Outfile_MLPred_HomDet_2to5GeV_FineGran128_V1.h5"]]
+algorithms = [#["Pflow","PflowPred","/afs/cern.ch/work/f/fdibello/public/Outfile_2to5GeV_TotalEpred.h5"],
+              #["CNN"  ,"RealRes_NeutralEnergyPred_Layer","/afs/cern.ch/work/s/sanmay/public/Outfile_MLPred_HomDet_2to5GeV_FineGran128_V1.h5"]]
+              ["DeepSet"  ,"RealRes_NeutralEnergyPred_Layer","/eos/user/s/sanmay/public/MULTIPLE_TRAINING/Outfile_DeepSet_HomDet_15to20GeV_TopoCluster_WS.h5"],
+              ["Graph"  ,"RealRes_NeutralEnergyPred_Layer","/eos/user/s/sanmay/public/MULTIPLE_TRAINING/Outfile_Graph_HomDet_15to20GeV_TopoCluster_WS.h5"],
+              ["Unet"  ,"RealRes_NeutralEnergyPred_Layer","/eos/user/s/sanmay/public/MULTIPLE_TRAINING/Outfile_UNet_HomDet_15to20GeV_TopoCluster_WS.h5"]]
 
-TestFile = "/afs/cern.ch/work/s/sanmay/public/Outfile_CellInformation_HomDet_2to5GeV_FineGran128_V1.h5"
-TCFile = "/afs/cern.ch/work/f/fdibello/Outfile_2to5GeV_TotalTopo.h5"
-Events = 1000
-
+#TestFile = "/eos/user/s/sanmay/public/MULTIPLE_TRAINING/Outfile_ConvNet_HomDet_5to10GeV_TopoCluster_WS.h5"
+TestFile = "/afs/cern.ch/work/s/sanmay/public/Outfile_CellInformation_HomDet_15to20GeV_FineGran128_V1.h5"
+TCFile = "/eos/user/s/sanmay/public/MULTIPLE_TRAINING/TopoClusterFile_WS.h5"
+#TCFile = "/afs/cern.ch/work/f/fdibello/Outfile_2to5GeV_TotalTopo.h5"
+Events = 5999
+PflowPred = ""
 
 
 # General Config file
@@ -27,66 +32,66 @@ gT     = h5py.File(TCFile,'r')
 
 
 #General track parameters
-C1=f["Trk_Theta"]
-C2=f["Trk_Phi"]
-C3=f["Trk_X_pos"]
-C4=f["Trk_Y_pos"]
 layers=[64,32,32,16,16,8]
-print algorithms[0][2]
+print(algorithms[0][2])
 gPF = h5py.File(algorithms[0][2],'r')
 
-
+print("Keys: %s" % gPF.keys())
 
 Fpred = []
 Hpred = []
 for algo in algorithms:
- print "Running with algo: ",algo[0] 
+ print("Running with algo: ",algo[0]) 
  # Add here the histograms
  gEpred = h5py.File(algo[2],'r')
  Fpred.append([gEpred,algo[1]])
  #Figure of Merit used for energy distribution within the TC
- h_pred =  ROOT.TH1D("h_Energy"+algo[0],"h_Energy"+algo[0],50,-2,2)
+ h_pred =  ROOT.TH1D("h_Energy"+algo[0],"h_Energy"+algo[0],400,-2,2)
  Hpred.append(h_pred)
 
  out_image = []
 for ev in range(Events):
- if ev%200 == 0:print "Analyzed:", ev
+ if ev%200 == 0:print("Analyzed:", ev)
  Etot = 0
  Eneutral = 0
  Ech = 0
- En=f["Smeared_Track_Energy"][ev]
- ptr=math.sqrt(En*En-139.57018*139.57018)
- Traj_Mark=Track.MakeTruthTrajectory(C1[ev], C2[ev], C3[ev], C4[ev])
+ #En=f["Smeared_Track_Energy"][ev]
+ #ptr=math.sqrt(En*En-139.57018*139.57018)
+ #Traj_Mark=Track.MakeTruthTrajectory(C1[ev], C2[ev], C3[ev], C4[ev])
  total=[]
  TC = -1
  FigureOfMerit = []
  for algo in algorithms:
   FigureOfMerit.append(0)
 
- for lay in range(6):
-    Etopo_t =gPF["PflowTC"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
-    for X in range(layers[lay]):
-      for Y in range(layers[lay]):
-       if Etopo_t[X][Y] != 0: 
-        TC = Etopo_t[X][Y] 
-        break 
-
+#This needs to be fixed
+# for lay in range(6):
+#    Etopo_t =gPF["PflowTC"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
+#    for X in range(layers[lay]):
+#      for Y in range(layers[lay]):
+#       if Etopo_t[X][Y] != 0: 
+#        TC = Etopo_t[X][Y] 
+#        break 
+# This is nasty and hard coded
+ TC = 1
  # This is the total energy of each prediction per event
  E = []
  for lay in range(6):
-     Traj_Mark[lay][0]= Traj_Mark[lay][0] -1 
-     Traj_Mark[lay][1]= Traj_Mark[lay][1] -1 
-     Ch_Ev=f["RealRes_ChargedEnergy_Layer"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
-     Neu_Ev=f["RealRes_NeutralEnergy_Layer"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
-     Lay_Ev=f["RealRes_TotalEnergy_Layer"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
-     Topo_Ev =gT["TopoClusters"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
+     #Traj_Mark[lay][0]= Traj_Mark[lay][0] -1 
+     #Traj_Mark[lay][1]= Traj_Mark[lay][1] -1 
+     #Ch_Ev=f["RealRes_ChargedEnergy_Layer"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
+     Neu_Ev=gEpred["RealRes_NeutralEnergyTruth_Layer"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
+     Lay_Ev=gEpred["RealRes_TotalEnergy_Layer"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
+     Topo_Ev =gT["topocluster_15to20"][ev][lay:lay+1,:layers[lay],:layers[lay]]
+     Ch_Ev = Lay_Ev - Neu_Ev
      Epred = []
      for pred in Fpred:
       Epred_Ev =pred[0][pred[1]+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
       Epred.append(Epred_Ev)
      for X in range(layers[lay]):
        for Y in range(layers[lay]):
-        if Topo_Ev[X][Y] == TC and TC != 0:
+        if Topo_Ev[0][X][Y] >  0 and TC != 0:
+        #if Topo_Ev[0][X][Y] == TC and TC != 0:
           Etot = Lay_Ev[X][Y] + Etot
           Eneutral = Neu_Ev[X][Y] + Eneutral
           Ech = Ch_Ev[X][Y] + Ech
@@ -99,7 +104,8 @@ for ev in range(Events):
  for pre in Epred:
   i = i + 1
   if  Ech!=0 and FigureOfMerit[i]!= 0 and Etot != 0 and Eneutral != 0 and int(FigureOfMerit[i]) != int(Etot):
-   if i == 0: Hpred[i].Fill(((Etot-FigureOfMerit[i]) - Eneutral)/Eneutral)   
+   #Very coarse wayif i == 0: Hpred[i].Fill(((Etot-FigureOfMerit[i]) - Eneutral)/Eneutral)   
+   if algo[0] == "Pflow": Hpred[i].Fill(((Etot-FigureOfMerit[i]) - Eneutral)/Eneutral)  
    else: Hpred[i].Fill(((FigureOfMerit[i]) - Eneutral)/Eneutral)   
 f_o=ROOT.TFile("Epred_outALL.root","recreate") 
 for h_pred in Hpred:
--- DATA_PROCESSING/Evaluate_TCPos.py
+++ DATA_PROCESSING/Evaluate_TCPos.py
@@ -10,20 +10,19 @@ import math
 import Track
 
 
-Events = 1000
+Events = 5999
 
-f = h5py.File('/afs/cern.ch/work/s/sanmay/public/Outfile_CellInformation_HomDet_2to5GeV_FineGran128_V1.h5','r')
+f = h5py.File('/afs/cern.ch/work/s/sanmay/public/Outfile_CellInformation_HomDet_2to5GeV_FineGran128_V1.h5')
 fnn = h5py.File('/afs/cern.ch/work/s/sanmay/public/Outfile_MLPred_HomDet_2to5GeV_FineGran128_V1.h5','r')
 
-print f.keys()
 
-gT     = h5py.File('Outfile_2to5GeV_TotalTopo.h5','r')
-gTnn     = h5py.File('Outfile_2to5GeV_TotalTopo.h5','r')
+gT     = h5py.File('/eos/user/f/fdibello/Outfile_2to5GeV_TotalTopo.h5','r')
+gTnn     = h5py.File('/eos/user/f/fdibello/Outfile_2to5GeV_TotalTopo.h5','r')
 #gT     = h5py.File('Outfile_2to5GeV_ChargedTopo.h5','r')
-gEpred = h5py.File('Outfile_2to5GeV_TotalEpred.h5','r')
+gEpred = h5py.File('/afs/cern.ch/work/l/losanti/public/forFrancesco/Outfile_2to5GeV_TotalEpred_WS_V1.h5','r')
 
 #h_Epred_prof_tot= ROOT.TProfile2D("h_Epred_prof_tot","h_Epred_prof_tot",5000000,1.5e3,5.5e3,6,0,6) 
-h_Epred_prof_tot= ROOT.TProfile2D("h_Epred_prof_tot","h_Epred_prof_tot",50,1.5e3,5.5e3,6,0,6) 
+#h_Epred_prof_tot= ROOT.TProfile2D("h_Epred_prof_tot","h_Epred_prof_tot",50,1.5e3,5.5e3,6,0,6) 
 
 C1=f["Trk_Theta"]
 C2=f["Trk_Phi"]
@@ -51,10 +50,10 @@ ED_ch_Plot = []
 Diff_X = []
 Diff_Y = []
 for lay in range(6):
-  print lay
+  print(lay)
   h_n_dR =  ROOT.TH1D("h_n_dR"+str(lay),"h_n_dR"+str(lay),100,-5,80)
   dR_n_Plot.append(h_n_dR)
-  h_p_dR =  ROOT.TH1D("h_p_dR"+str(lay),"h_p_dR"+str(lay),100,-5,80)
+  h_p_dR =  ROOT.TH1D("h_p_dR"+str(lay),"h_p_dR"+str(lay),410,-1,40)
   dR_p_Plot.append(h_p_dR)
   h_t_dR =  ROOT.TH1D("h_t_dR"+str(lay),"h_t_dR"+str(lay),100,-5,80)
   dR_t_Plot.append(h_t_dR)
@@ -99,18 +98,13 @@ h_Eneutral =  ROOT.TH1D("h_Eneutral","h_Eneutral",200,-500,10000)
 
 f1 = ROOT.TFile.Open("Epred_outN.root")
 #f1 = ROOT.TFile.Open("fpred_new_final.root")
-hProf_t = f1.Get("h_Epred_prof_tot")
+#hProf_t = f1.Get("h_Epred_prof_tot")
 layers=[64,32,32,16,16,8]
 out_image = []
 #for ev in range(4,5):
 # print ev
 for ev in range(Events):
- if ev%200 == 0:print ev
- EPflow = 0
- Etot = 0
- Eneutral = 0
- Ech = 0
- Enn = 0
+ if ev%200 == 0:print(ev)
  En=f["Smeared_Track_Energy"][ev]
  ptr=math.sqrt(En*En-139.57018*139.57018)
  Traj_Mark=Track.MakeTruthTrajectory(C1[ev], C2[ev], C3[ev], C4[ev])
@@ -142,7 +136,6 @@ for ev in range(Events):
       Neu_Ev=f["RealRes_NeutralEnergy_Layer"+str(lay+1)][ev][0]
       Lay_Ev=f["RealRes_TotalEnergy_Layer"+str(lay+1)][ev][0]
       ENN_Ev=fnn["RealRes_NeutralEnergyPred_Layer"+str(lay+1)][ev][0]
-      #Etopo_Ev =gEpred["PflowTC"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
       Etopo_Ev =gEpred["PflowTC"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
       Topo_Ev =gT["TopoClusters"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
       TopoNN_Ev =gTnn["TopoClusters"+str(lay+1)][ev][0][:layers[lay],:layers[lay]]
@@ -161,22 +154,26 @@ for ev in range(Events):
       yAn = 0
       xAt = 0
       yAt = 0
+      EPflow = 0
+      Etot = 0
+      Eneutral = 0
+      Ech = 0
+      Enn = 0
       
       for X in range(layers[lay]):
         for Y in range(layers[lay]):
          cut = False
          if (Ch_Ev[X][Y]==0. and Neu_Ev[X][Y]>=0)  and   (ENN_Ev[X][Y] <0.5*Neu_Ev[X][Y] and ENN_Ev[X][Y]>0.5 and Neu_Ev[X][Y]>150.): cut = True
 
-         if cut==False: continue
+      #   if cut==False: continue
 
- 
-         #Truth
-         Eneutral = Neu_Ev[X][Y] + Eneutral
-         xAt = X* Neu_Ev[X][Y] + xAt
-         yAt = Y* Neu_Ev[X][Y] + yAt
-         ED_t_Plot[lay].Fill(X-Traj_Mark[lay][0],Y-Traj_Mark[lay][1],Neu_Ev[X][Y])
-         ED_tot_not_Plot[lay].Fill(X-Traj_Mark[lay][0],Y-Traj_Mark[lay][1],Lay_Ev[X][Y])
-         if    Topo_Ev[X][Y] == TC:
+         if Etopo_Ev[X][Y] == TC:
+           #Truth
+           Eneutral = Neu_Ev[X][Y] + Eneutral
+           xAt = X* Neu_Ev[X][Y] + xAt
+           yAt = Y* Neu_Ev[X][Y] + yAt
+           ED_t_Plot[lay].Fill(X-Traj_Mark[lay][0],Y-Traj_Mark[lay][1],Neu_Ev[X][Y])
+           ED_tot_not_Plot[lay].Fill(X-Traj_Mark[lay][0],Y-Traj_Mark[lay][1],Lay_Ev[X][Y])
   #         if (Neu_Ev[X][Y]+Ch_Ev[X][Y])!=0: print Neu_Ev[X][Y]/(Neu_Ev[X][Y]+Ch_Ev[X][Y]), Neu_Ev[X][Y], ENN_Ev[X][Y]
            if Neu_Ev[X][Y] > 0 and Neu_Ev[X][Y]/(Ch_Ev[X][Y]+Neu_Ev[X][Y]) >0.9: ED_fraction_Plot[lay].Fill(X-Traj_Mark[lay][0],Y-Traj_Mark[lay][1], (ENN_Ev[X][Y]-Neu_Ev[X][Y])/Neu_Ev[X][Y])
            if Neu_Ev[X][Y] > 0 and Neu_Ev[X][Y]/(Ch_Ev[X][Y]+Neu_Ev[X][Y]) <0.15: ED_fraction_Plot1[lay].Fill(X-Traj_Mark[lay][0],Y-Traj_Mark[lay][1], (ENN_Ev[X][Y]-Neu_Ev[X][Y])/Neu_Ev[X][Y])
@@ -211,7 +208,7 @@ for ev in range(Events):
 
 
       if Etot!= 0:h_fraction.Fill(Eneutral/Etot) 
-      if EPflow == 0 or Eneutral == 0 or Enn == 0: continue
+      if EPflow == 0:  continue
      
       xAp = xAp / (Etot-EPflow)
       xAt = xAt / Eneutral
@@ -241,7 +238,7 @@ for ev in range(Events):
 
 
 
-f_o=ROOT.TFile("Epred_out_Position.root","recreate") 
+f_o=ROOT.TFile("Epred_out_Position_Pflow.root","recreate") 
 for ly in range(6):
  dR_p_Plot[ly].Write()
  dR_n_Plot[ly].Write()
